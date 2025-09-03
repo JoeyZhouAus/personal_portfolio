9df2e1dcde09dc8d787279f6e1fcf2ce9e63dbbb
joeyzhou <joeyzhouaus@gmail.com>
2025-09-03 23:50:17 +1000

    update with personal resume data regarding work experiences and university information

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -24,17 +24,12 @@ const social: Social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system",
+    link: "https://github.com/JoeyZhouAus/personal_portfolio",
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
-  },
-  {
-    name: "Threads",
-    icon: "threads",
-    link: "https://www.threads.com/@once_ui",
+    link: "https://www.linkedin.com/in/joeyzhouinaustralia/",
   },
   {
     name: "Email",
@@ -103,52 +98,104 @@ const about: About = {
     ),
   },
   work: {
-    display: true, // set to false to hide this section
+    display: true,
     title: "Work Experience",
     experiences: [
       {
         company: "PEXA",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        timeframe: "01/2023 – present",
+        role: "Engineer",
         achievements: [
-          <>
-            Developed features for the PEXA core exchange app, which streamlined digital property settlement processes across various jurisdictions in Australia, making transactions smoother for users.
-          </>,
-          <>
-            Built APIs for containerised applications in Spring Boot using Java and Kotlin, helping clients integrate our API to enhance their banking systems and achieve a fully streamlined digital settlement experience.
-          </>,
-          <>
-          Enhanced support for internal teams by developing new features on developer portals and facilitating API gateway migrations using Kong gateway, which significantly improved overall system efficiency.
-          </>,
-          <>
-          Collaborated with lead engineers on the migration project to the Kong gateway, ensuring a seamless transition that boosted API performance and reliability.
-          </>,
-          <>
-          Conducted extensive automation testing using specialised scripts, which validated the migration's efficacy and led to a remarkably high success rate, ultimately enhancing system reliability.
-          </>,
-          <>
-          Mentored and supervised junior team members, fostering their professional growth and development, which strengthened our team's capabilities and overall performance.
-          </>,
+          <>Developed features for the PEXA core exchange app, which streamlined digital property settlement processes across various jurisdictions in Australia, making transactions smoother for users.</>,
+          <>Built APIs for containerised applications in Spring Boot using Java and Kotlin, helping clients integrate our API to enhance their banking systems and achieve a fully streamlined digital settlement experience.</>,
+          <>Enhanced support for internal teams by developing new features on developer portals and facilitating API gateway migrations using Kong gateway, which significantly improved overall system efficiency.</>,
+          <>Collaborated with lead engineers on the migration project to the Kong gateway, ensuring a seamless transition that boosted API performance and reliability.</>,
+          <>Conducted extensive automation testing using specialised scripts, which validated the migration's efficacy and led to a remarkably high success rate, ultimately enhancing system reliability.</>,
+          <>Mentored and supervised junior team members, fostering their professional growth and development, which strengthened our team's capabilities and overall performance.</>,
         ],
-        images: [
-          // optional: leave the array empty if you don't want to display images
-          {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
-            width: 16,
-            height: 9,
-          },
+        images: [],
+      },
+      {
+        company: "CoreLogic",
+        timeframe: "02/2022 – 12/2022",
+        role: "Professional Software Engineer (remote)",
+        achievements: [
+          <>API development that the majority of banks in Australia and New Zealand rely on regarding property valuation using Java 8, Hibernate, Spring MVC, and Postgres</>,
+          <>Continuous improvement on existing GitLab CI/CD pipeline</>,
+          <>Practicing Agile methodology with BDD</>,
+          <>Automation tests at the heart of development by writing unit tests, integration tests as well as acceptance tests plus maintaining and improving test coverage</>,
+          <>Working alongside internal testers or collaborating with stakeholders from different teams to investigate issues and find solutions with minimum impact</>,
+          <>Writing Load testing script to find bottlenecks of the system</>,
+          <>Supervised and trained junior team members and supporting them along the way</>,
         ],
+        images: [],
+      },
+      {
+        company: "ISOTON",
+        timeframe: "12/2020 – 01/2022",
+        role: "Software Engineer",
+        achievements: [
+          <>Backend API and Microservices development and work with external App development teams to support many transactions that have an impact on millions of users using Spring Boot</>,
+          <>Software releases onto private cloud servers and also the AWS platform through integrations with Bitbucket pipeline and Circle CI/CD or manual deployment</>,
+          <>Participating in all phases of the software development cycle including requirements analysis, design, implementation, testing, and post-production support for mobile virtual network owners like TPG Mobile, Lebara, Kogan Mobile, and Felix Mobile</>,
+          <>Participating in project planning, providing realistic time estimates for units of work, updating and tracking progress using Jira</>,
+          <>Ability to handle highly stressed situations and unexpected errors efficiently</>,
+          <>Working independently to meet tight deadlines in a dynamic team environment</>,
+        ],
+        images: [],
+      },
+      {
+        company: "Virtual Agent",
+        timeframe: "11/2019 – 12/2020",
+        role: "Software Engineer",
+        achievements: [
+          <>Developing real estate maintenance solutions that provide a hassle-free communications platform through Landlord, Property managers, tradies, and tenants regarding maintenance work</>,
+          <>API Integrations with Third Parties like PropertyMe and PropertyTree software platform</>,
+          <>Deploying software releases onto the Azure platform</>,
+          <>Requirements gatherings and analysis with stakeholders and users directly to maintain and create new features for the existing platform</>,
+          <>Responsible for new project design and architecture with other developers</>,
+          <>Developed a Mobile App internally for property managers to book and conduct inspections</>,
+        ],
+        images: [],
+      },
+      {
+        company: "ISOTON",
+        timeframe: "11/2017 – 10/2019",
+        role: "Software Engineer",
+        achievements: [
+          <>Developed and enhanced software for ISOTON, including customer portal, CRM portal, and frontend and backend databases, catering to clients like TPG Mobile, Lebara, Kogan Mobile, and Felix Mobile.</>,
+          <>Collaborated with team members to manage complex projects for corporate clients, impacting a large user base.</>,
+          <>Demonstrated independent work ability and met strict deadlines in a dynamic team environment.</>,
+          <>Prepared and executed comprehensive Dev Test cases.</>,
+          <>Coordinated with various departments to ensure smooth project implementation.</>,
+          <>Presented completed projects to stakeholders in a polished and professional manner.</>,
+          <>Generated code build/release for deployment in multiple environments.</>,
+          <>Created BAU and bulk update scripts to address system issues with minimum impact on customers</>,
+          <>Provided post-deployment care for projects.</>,
+        ],
+        images: [],
+      },
+      {
+        company: "ISOTON",
+        timeframe: "03/2017 – 11/2017",
+        role: "Software Q&A Engineer",
+        achievements: [
+          <>Carrying out Manual testing (black box testing, usability, user interface, ad-hoc) to discover errors and optimize usability</>,
+          <>Creating configurable automation tests using Nightwatch Framework according to different projects for UI Testing</>,
+          <>Preparing Manual test cases before testing</>,
+          <>Assess and highlight potential project risks and suggest corrective actions or alternatives.</>,
+        ],
+        images: [],
       },
     ],
   },
   studies: {
-    display: true, // set to false to hide this section
-    title: "Studies",
+    display: true,
+    title: "Education",
     institutions: [
       {
-        name: "University of South Australia",
-        description: <>Studied Computer Science.</>,
+        name: "University Of South Australia",
+        description: <>Bachelor of Information Technology (Software Development) • 03/2013 – 12/2015</>,
       },
     ],
   },
